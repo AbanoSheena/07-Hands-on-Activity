7bbddd0b6ae56a29380f5577345d8a15f9d4dc7c
AbanoSheena <116648308+AbanoSheena@users.noreply.github.com>
2022-11-23 07:57:20 +0800

    Add files via upload

---
+++ Abano 07 Hands on Activity/Dog.java
@@ -0,0 +1,11 @@
+class Dog extends Animal {
+	public void eat (){
+		System.out.print("Dog love to eat dog foods ");
+	}
+	public void sleep(){
+		System.out.println("and sleep for 10-12 hours a day.");
+	}
+	public void makeSound(){
+		System.out.println("Bark Bark");
+	}
+}
